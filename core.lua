d682e16a4b5a1caf2f58ae6b132c01fd3303bc7d
Bear <programming@hallhome.us>
2022-12-16 11:41:26 -0600

    rollback

--- core.lua
+++ core.lua
@@ -88,7 +88,7 @@ for key, value in ipairs(addonTable.expansion) do
 		AllFilters(db)
 	end
 end
---[[
+
 -- Now process global databases
 for key, value in pairs(addonTable.Globaldbs) do
 	db = addonTable[value]
@@ -96,4 +96,3 @@ for key, value in pairs(addonTable.Globaldbs) do
 		AllFilters(db)
 	end
 end
---]]
